8733ed2c3248a7f25e3fec5b4e0dc7a85392ea09
na-me <robwobbol@gmail.com>
2014-03-05 18:06:10 -0600

    added hero.cpp. added current binary. removed cruft.

---
+++ hero.cpp
@@ -0,0 +1,17 @@
+typedef struct{
+//use an enum for these states
+walkingLeft;
+walkingRight;
+attacking;
+jumping;
+takingDamage;
+dieing;
+
+attackDamage;
+movementSpeed;
+range;
+
+int  createHero(SDL_Texture* spriteSheet,std::string* name)
+{
+heroState name;
+name->walking
